ee330ea297614699073f5f3ea04ba125eda33a25
punnu05 <76902769+punnu05@users.noreply.github.com>
2023-01-10 11:58:16 +0530

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApi.Models
+{
+    public class Employee
+    {
+        public long EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public string Department { get; set; }
+        public string Email { get; set; }
+        public DateTime? DOJ { get; set; }
+    }
+}
